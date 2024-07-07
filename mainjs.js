b0922e56a9d2672abd6cae1488d7f8d265cedd41
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-07 10:36:50 +0100

    addicng css breakpoint for mobiles.

--- mainjs.js
+++ mainjs.js
@@ -6,7 +6,7 @@ function startClock(city) {
     clearInterval(intervalId);
   }
 
-  // Function to show the current time
+  // This switch case shows the ime in the selected time zone.
   function showTime() {
     let timeZone;
     switch (city) {
@@ -29,21 +29,21 @@ function startClock(city) {
         console.error('Unknown city:', city);
         return; // Exit the function if the city is unknown
     }
-
+        // This object contains the options which for the make up of the date and it is passed as an argument to the timeString constant in the toLocaleTimestring()
     const options = { 
       timeZone: timeZone, 
       hour12: false, 
       hour: 'numeric', 
       minute: 'numeric', 
-      second: 'numeric' // Add seconds for real-time updates
+      second: 'numeric' 
     };
     const timeString = new Date().toLocaleTimeString('en-US', options);
-
+//The line below formats the string using back tick and $ sign concatination.
     document.getElementById('timeDisplay').textContent = `Current time in ${city}: ${timeString}`;
   }
 
-  // Show the time immediately
+  // calling the function below shows the time immediatly.
   showTime();
-  // Set an interval to update the time every second
+  // Set an interval to update the time every second and make the clock tick.
   intervalId = setInterval(showTime, 1000);
 }
